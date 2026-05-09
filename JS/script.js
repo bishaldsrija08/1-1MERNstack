60950503281dd958649f1f86e443ec1bb289a6c1
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-05-09 22:12:28 +0545

    Add HTML structure and JavaScript functionality for bulb toggle feature

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+// node --watch fileName.js
+// console.log("Bishal Rijal123")
+// console.log(9+9)
+// console.log("learning JavaScript")
+
+/*console.log("Hello, World!")
+x = 6
+y=7
+console.log(x+y)*/
+
+console.log("Bishal Rijal is a MERN stack trainer!")
