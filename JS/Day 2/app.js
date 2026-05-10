ca542d414f7311ee0bc9ce1a5b5b62ba4a1a64cf
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-05-10 21:53:58 +0545

    js basics

---
+++ JS/Day 2/app.js
@@ -0,0 +1,71 @@
+console.log("Hello, World!"); // String are the sequence of characters. They are used to represent text. They are enclosed in single quotes, double quotes, or backticks.
+
+console.log(3+8) // Number are the data type that represents numeric values. They can be integers or floating-point numbers.
+
+// Concatenation is the process of combining two or more strings together. In JavaScript, you can use the + operator to concatenate strings.
+
+console.log("Bishal " + "Rijal" + " is a developer.") // Output: "Bishal Rijal is a developer."
+
+console.log(9+9) // Arithmetic operators are used to perform mathematical operations on numbers. The + operator is used for addition, - for subtraction, * for multiplication, / for division, and % for modulus (remainder).
+
+// "Bishal"
+
+console.log("9"+"9")
+console.log("1"+"0")
+console.log("Bishal "+"Rijal")
+
+console.log("9")
+
+// console.log(a)
+// Arithmetic operators are used to perform mathematical operations on numbers. The + operator is used for addition, - for subtraction, * for multiplication, / for division, and % for modulus (remainder).
+console.log(1+2)
+console.log(2-1)
+console.log(2*3)
+console.log(6/3)
+console.log(9%5) // % is a modulus operator
+console.log(3**10) // ** is an exponentiation operator
+
+console.log(9/0) // Output: Infinity
+console.log(0/0) // Output: NaN (Not a Number)
+
+
+console.log((2+2)*5)
+console.log(3/1 + 2**2)
+
+num = 10
+num +=10 // num = num + 10
+console.log(num)
+
+// Comparison operators are used to compare two values. They return a boolean value (true or false). The == operator is used for equality, != for inequality, > for greater than, < for less than, >= for greater than or equal to, and <= for less than or equal to.
+
+console.log(9==10) // Output: false
+console.log(9!=10) // Output: true
+console.log(9>10) // Output: false
+console.log(9<10) // Output: true
+console.log(9>=10) // Output: false
+console.log(9<=10) // Output: true
+console.log(9>=9) // Output: true
+console.log(9<=9) // Output: true
+
+firstName = "Bishal" // JavaScript
+lastName = "Rijal"
+myAge = 25
+
+first_name = "Bishal" // python
+last_name = "Rijal"
+
+FirstName = "Bishal" // Java
+my_age = 25 // python
+// first-name = "Bishal" // Invalid variable name in JavaScript, cannot contain hyphens
+
+num = 34
+console.log(num)
+
+console.log("Bishal","Rijal", num)
+
+num1=7
+num2=8
+console.log(num1+num2)
+
+firstName = "Bishal"
+console.log(firstName) // This will throw an error because print is not defined in JavaScript. The correct way to print in JavaScript is to use console.log().
