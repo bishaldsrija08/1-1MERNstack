e4cf65e68daaa58b787b1ebd82ca0713d6c5cefe
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-05-21 21:17:28 +0545

    Refactor blog routes for consistency and add update blog functionality

--- bmsProject/server/app.js
+++ bmsProject/server/app.js
@@ -33,7 +33,7 @@ app.post("/blogs", async (req, res) => {
 })
 
 // Get all blogs
-app.get("/blogs", async (req, res)=>{
+app.get("/blogs", async (req, res) => {
     const blogs = await Blog.find()
     res.status(200).json({
         message: "Blogs retrieved successfully",
@@ -43,10 +43,10 @@ app.get("/blogs", async (req, res)=>{
 
 // Single get
 
-app.get("/blogs/:id", async (req, res)=>{
+app.get("/blogs/:id", async (req, res) => {
     const id = req.params.id
     const singleBlog = await Blog.findById(id)
-    if (!singleBlog){
+    if (!singleBlog) {
         return res.status(404).json({
             message: "Blog not found"
         })
@@ -58,7 +58,7 @@ app.get("/blogs/:id", async (req, res)=>{
 })
 
 // Delete a blog
-app.delete("/blogs/:id", async (req, res)=>{
+app.delete("/blogs/:id", async (req, res) => {
     const id = req.params.id
     await Blog.findByIdAndDelete(id)
     res.status(200).json({
@@ -67,6 +67,30 @@ app.delete("/blogs/:id", async (req, res)=>{
 })
 
 // Update a blog
+app.patch("/blogs/:id", async (req, res) => {
+    const id = req.params.id // get the id from the url parameters
+
+    const blogTitle = req.body.blogTitle
+    const blogSubTitle = req.body.blogSubTitle
+    const blogDescription = req.body.blogDescription
+
+    if (!blogTitle || !blogSubTitle || !blogDescription) {
+        return res.status(400).json({
+            message: "All fields are required"
+        })
+    }
+
+    await Blog.findByIdAndUpdate(id, {
+        blogTitle,
+        blogSubTitle,
+        blogDescription
+    })
+
+    res.status(200).json({
+        message: "Blog updated successfully"
+    })
+
+})
 
 // Server starting
 const port = 3000
@@ -74,10 +98,3 @@ const port = 3000
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
-
-
-/*
-const blogs = ["blog1", "blog2", "blog3"]
-
-    
-*/
