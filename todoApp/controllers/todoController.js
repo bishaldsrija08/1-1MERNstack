0e0c5ee3c9da42fc48238e341ae198cf2803b6cf
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-05-22 22:45:07 +0545

    Implement CRUD operations for tasks: add getAllTasks, getSingleTask, updateTask, and deleteTask functionalities

--- todoApp/controllers/todoController.js
+++ todoApp/controllers/todoController.js
@@ -17,14 +17,52 @@ const createTask = async (req, res) => {
     res.status(201).json({ message: "Task created successfully" });
 }
 // Get all tasks
+const getAllTasks = async (req, res)=>{
+    const tasks = await Todo.find();
+    res.status(200).json({data: tasks, message: "Tasks fetched successfully"});
+}
 
 // Get single task
+const getSingleTask = async (req, res)=>{
+    const id = req.params.id;
+    const task = await Todo.findById(id);
+    if(!task){
+        return res.status(404).json({message: "Task not found"});
+    }
+
+    res.status(200).json({data: task, message: "Task fetched successfully"});
+
+}
 
 // Update task
+const updateTask = async (req, res)=>{
+    const id = req.params.id;
+    const { title, description, completed } = req.body;
+
+    if (!title || !description) {
+        return res.status(400).json({ message: "Title and description are required" });
+    }
+
+    await Todo.findByIdAndUpdate(id, {
+        title,
+        description,
+        completed
+    })
+    res.status(200).json({ message: "Task updated successfully" });
+}
 
 // Delete task
+const deleteTask = async (req, res)=>{
+    const id = req.params.id;
+    await Todo.findByIdAndDelete(id);
+    res.status(200).json({ message: "Task deleted successfully" });
+}
 
 
 module.exports = {
-    createTask
+    createTask,
+    getAllTasks,
+    getSingleTask,
+    updateTask,
+    deleteTask
 }
--- todoApp/routes/taskRoutes.js
+++ todoApp/routes/taskRoutes.js
@@ -1,8 +1,9 @@
-const { createTask } = require('../controllers/todoController');
+const { createTask, getAllTasks, getSingleTask, updateTask, deleteTask } = require('../controllers/todoController');
 
 const router = require('express').Router();
 
-router.route("/tasks").post(createTask);
+router.route("/tasks").post(createTask).get(getAllTasks);
+router.route("/tasks/:id").get(getSingleTask).patch(updateTask).delete(deleteTask)
 
 
 
