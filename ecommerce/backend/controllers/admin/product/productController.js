72b1dff6902266a45b2b352dbf97723a0368d718
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-06-20 21:22:41 +0545

    Implement CRUD operations for product management

--- ecommerce/backend/controllers/admin/product/productController.js
+++ ecommerce/backend/controllers/admin/product/productController.js
@@ -17,17 +17,67 @@ const createProduct = async (req, res) => {
     })
 
     return res.status(201).json({ message: "Product created successfully" });
-
-
 }
+
 // get all products
+const getAllProducts = async (req, res) => {
+    const products = await Product.find();
+    return res.status(200).json({
+        message: "Products fetched successfully",
+        data: products
+    });
+}
 
 // get single product
+const getSingleProduct = async (req, res) => {
+    const id = req.params.id;
+    const product = await Product.findById(id);
+    if (!product) {
+        return res.status(404).json({ message: "Product not found" });
+    }
+    return res.status(200).json({
+        message: "Product fetched successfully",
+        data: product
+    });
+}
 
 // update product
+const updateSingleProduct = async (req, res) => {
+    const id = req.params.id;
+    const { productName, productDescription, productStockQty, productStatus, productPrice } = req.body;
+    if (!productName || !productDescription || !productStockQty || !productStatus || !productPrice) {
+        return res.status(400).json({ message: "All fields are required" });
+    }
+    const product = await Product.findById(id);
+    if (!product) {
+        return res.status(404).json({ message: "Product not found" });
+    }
 
+    await Product.findByIdAndUpdate(id, {
+        productName,
+        productDescription,
+        productStockQty,
+        productStatus,
+        productPrice,
+        productImageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNxCYXDiJ6LUG-qyO4XnRqqiB8pLwDQvHmKye1Y6ib5A&s=10"
+    })
+    res.status(200).json({ message: "Product updated successfully" })
+}
 // delete product
+const deleteSingleProduct = async (req, res) => {
+    const id = req.params.id;
+    const product = await Product.findById(id);
+    if (!product) {
+        return res.status(404).json({ message: "Product not found" });
+    }
+    await Product.findByIdAndDelete(id);
+    return res.status(200).json({ message: "Product deleted successfully" });
+}
 
 module.exports = {
-    createProduct
+    createProduct,
+    getAllProducts,
+    getSingleProduct,
+    updateSingleProduct,
+    deleteSingleProduct
 }
--- ecommerce/backend/routes/admin/product/productRoutes.js
+++ ecommerce/backend/routes/admin/product/productRoutes.js
@@ -1,12 +1,12 @@
-const { createProduct } = require('../../../controllers/admin/product/productController');
+const { createProduct, getAllProducts, getSingleProduct, updateSingleProduct, deleteSingleProduct } = require('../../../controllers/admin/product/productController');
 
 const router = require('express').Router();
 
 
-
+// Restful API routes for product management
 router.route("/create").post(createProduct)
-
-
+router.route("/products").get(getAllProducts)
+router.route("/products/:id").get(getSingleProduct).patch(updateSingleProduct).delete(deleteSingleProduct)
 
 
 module.exports = router
